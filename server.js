53c7581bb5ba30419b1e2b56ec656dbafa6afc90
Chaudhary YogeshKumar <yogeshchaudhary9081@gmail.com>
2023-12-22 11:26:54 +0530

    Origin change

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 const corsOptions = {
-  origin: "http://localhost:5173",
+  origin: "https://mernapp-yogesh.netlify.app",
   methods: "GET, POST, PUT, DELETE,PATCH,HEAD",
   credentials: true,
 };
